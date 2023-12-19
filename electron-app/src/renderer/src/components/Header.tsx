eed00204a786b1ecc25921fa999df41ae918b52f
demattosanthony <demattosanthony@gmail.com>
2023-12-19 16:46:32 -0500

    removed electron app

--- electron-app/src/renderer/src/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-import '@renderer/assets/index.css'
-
-export default function Header() {
-  return <div className="w-full h-10 min-h-[40px] bg-secondary no-select app-drag"></div>
-}
--- electron-app/src/renderer/src/screens/SplashScreen.tsx
+++
@@ -1,9 +0,0 @@
-import keyImg from '../assets/keyLogoWhite1.png'
-
-export function SplashScreen() {
-  return (
-    <div className="h-screen w-screen flex flex-col gap-2 items-center justify-center bg-black">
-      <img src={keyImg} height={250} width={250} className="animate-bounce" />
-    </div>
-  )
-}
