f7586e644aea892d404829795cd7ac0c13ad778e
demattosanthony <demattosanthony@gmail.com>
2023-12-21 21:56:31 -0500

    removed web frontend

--- web-frontend/src/pages/AiChatPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from "@/components/Chat";
-
-export default function AiChatPage() {
-  return (
-    <div className={`h-screen w-screen flex items-center p-8 gap-10 relative`}>
-      <Chat />
-    </div>
-  );
-}
