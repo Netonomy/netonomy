f5527c0654a5a9438c473e7ff8c6cd3b2473b19b
demattosanthony <demattosanthony@gmail.com>
2024-01-12 18:05:30 -0500

    slash command

--- tauri-app/src/pages/NoteEditorPage.tsx
+++ tauri-app/src/pages/NoteEditorPage.tsx
@@ -1,10 +1,11 @@
 import PageContainer from "@/components/PageContainer";
+import SlashCommand from "@/components/editor/extensions/SlashCommand";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 export default function NoteEditorPage() {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [SlashCommand, StarterKit],
     editorProps: {
       attributes: {
         class:
@@ -41,44 +42,17 @@ export default function NoteEditorPage() {
       <br />
       — Mom
     </blockquote>
-
-        <h1>Hello</h1>
-        <h2>Hello</h2>
-            <h3>Hello</h3>
-                <h4>Hello</h4>
-    <p>
-      this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-    </p>
-    <ul>
-      <li>
-        That’s a bullet list with one …
-      </li>
-      <li>
-        … or two list items.
-      </li>
-    </ul>
-    <p>
-      Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-    </p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-    <p>
-      I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-    </p>
-    <blockquote>
-      Wow, that’s amazing. Good work, boy! 👏
-      <br />
-      — Mom
-    </blockquote>
     `,
   });
 
   return (
     <PageContainer>
-      <div className="flex flex-1 w-full px-4 md:px-12 rounded-lg shadow-lg overflow-hidden  flex-col items-center justify-center dark:bg-black relative">
+      <div className="flex flex-1 w-full  pl-4 overflow-hidden  flex-col items-center justify-center dark:bg-black relative">
         {editor && (
-          <EditorContent editor={editor} className="h-full overflow-y-auto" />
+          <EditorContent
+            editor={editor}
+            className="h-full w-full overflow-y-auto"
+          />
         )}
       </div>
     </PageContainer>
