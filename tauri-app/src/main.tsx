1eb28603fb529e50eaced7a2b0d34057fd644bb2
demattosanthony <demattosanthony@gmail.com>
2024-01-15 00:30:35 -0500

    fixed pdf loading background colors

--- tauri-app/src/main.tsx
+++ tauri-app/src/main.tsx
@@ -1,10 +1,14 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Worker } from "@react-pdf-viewer/core";
 import "./globals.css";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <App />
-  // {/* </React.StrictMode> */}
+  <React.StrictMode>
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <App />
+    </Worker>
+  </React.StrictMode>
 );
