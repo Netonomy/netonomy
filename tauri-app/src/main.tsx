5c086c34aac8d24b0e28c2d2a2c6292a512e6a61
demattosanthony <demattosanthony@gmail.com>
2024-01-15 01:05:29 -0500

    pdfjs version

--- tauri-app/src/main.tsx
+++ tauri-app/src/main.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
       <App />
     </Worker>
   </React.StrictMode>
